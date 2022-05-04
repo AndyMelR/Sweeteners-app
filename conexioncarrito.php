c482a3352599560f13f71affbcd2d28cda15032e
soybastet <104266628+soybastet@users.noreply.github.com>
2022-05-04 21:32:06 +0200

    Delete conexioncarrito.php

--- conexioncarrito.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*conexion con bbdd*/
-$databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-?>
